b9ed66375a874528f69de9e4d0fde8b98346cefb
ArbazSk <ArbazSk@arbazshaikh513@gmail.com>
2018-09-17 19:25:48 +0530

    half Done

--- php/functions.php
+++ php/functions.php
@@ -3,8 +3,8 @@
 $link=null;
 function connect_db(){
     $db_host = "localhost";
-    $db_user = "root";
-    $db_password = "root";
+    $db_user = "";
+    $db_password = "";
     $db_name = "passmanager";
     global $link;
     $link = mysqli_connect($db_host,$db_user,$db_password) or die(mysqli_error());
--- php/loginCheck.php
+++ php/loginCheck.php
@@ -1,34 +1,4 @@
 <?php
 $link=null;
 
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "root";
-$db_name = "passmanager";
-
-$link = mysqli_connect($db_host,$db_user,$db_password,$db_name) or die(mysqli_error($link));
-if(!$link){
-    echo 'Connection Error';
-}
-else{
-    echo 'yep i done it<br>';
-}
-
-// mysqli_select_db($link,$db_name) or die(mysqli_error());
-$result =  mysqli_query($link,"SELECT USER_ID,UNAME,UPHONE FROM user WHERE USER_ID=1") ;
-if(!$result){
-    echo mysqli_error($link);
-}
-$data= mysqli_fetch_array($result);
-foreach($data as $value)
-{
-    print_r($value);
-    echo '<br>';
-
-}
-// var_dump($data);
-
-// phpinfo();
-
-
 ?>
