4c6c9686b6c434a67f0d7d14f198cba9c7c4a8cf
ArbazSk <ArbazSk@arbazshaikh513@gmail.com>
2018-09-19 23:25:09 +0530

    added

--- php/functions.php
+++ php/functions.php
@@ -3,13 +3,17 @@
 $link=null;
 function connect_db(){
     $db_host = "localhost";
-    $db_user = "";
-    $db_password = "";
+    $db_user = "root";
+    $db_password = "root";
     $db_name = "passmanager";
     global $link;
-    $link = mysqli_connect($db_host,$db_user,$db_password) or die(mysqli_error());
+    $link = mysqli_connect($db_host,$db_user,$db_password,$db_name) or die(mysqli_error($link));
 
-    mysqli_select_db($link,$db_name) or die(mysqli_error());
+    mysqli_select_db($link,$db_name) or die(mysqli_error($link));
+}
+function close_db(){
+  global $link;
+  mysqli_close($link);
 }
 
 function MyquerySelect($query){
@@ -17,7 +21,7 @@ function MyquerySelect($query){
     global $link;
     $result = mysqli_query($link,$query);
     if(!$result){
-        echo 'Somthing went wrong '.mysqli_error($link);
+        echo 'Somthing went wrong '.'<br>'.mysqli_error($link);
         die();
     }
     mysqli_close();
--- php/loginCheck.php
+++ php/loginCheck.php
@@ -1,4 +1,12 @@
 <?php
-$link=null;
+require_once 'functions.php';
 
+$usr = $_POST['username'];
+$pass = $_POST['pass'];
+$query = "SELECT * FROM master_account WHERE M_USERNAME = $usr AND M_PASSWORD = $pass";
+$result = MyquerySelect($query);
+if(!$result){
+  echo "No Valid User.";
+}
+var_dump($result);
 ?>
